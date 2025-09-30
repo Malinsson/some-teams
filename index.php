39be80d44f4919e15267f599dadf2504ec243d54
Malin Persson <malin.persson02@hotmail.com>
2025-09-30 20:25:54 +0200

    Constructed a foreach loop to display out data

--- index.php
+++ index.php
@@ -1,9 +1,28 @@
 <?php
 require_once __DIR__ . '/data.php';
 require_once __DIR__ . '/header.php';
+
 ?>
 
 <main>
+
+    <?php foreach ($teams as $key => $team): ?>
+        <h3><?= $key ?> </h3>
+        <img src="<?= $team['logo'] ?>" alt="The teams logo" width="250">
+        <p><?= $team['league'] ?></p>
+        <p><?= $team['city'] ?></p>
+        <p><?= $team['uefa-coefficient-ranking'] ?></p>
+        <p><?= $team['url'] ?></p>
+        <p>Opponents</p>
+        <?php foreach ($team['opponents'] as $opponent): ?>
+            <ul>
+                <li><?= $opponent ?></li>
+            </ul>
+        <?php endforeach ?>
+
+
+    <?php endforeach ?>
+
 
 </main>
 </body>
