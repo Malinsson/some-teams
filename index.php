79843a6fc69265d9da37ac8961d49c71d6664013
Malin Persson <malin.persson02@hotmail.com>
2025-10-02 11:56:30 +0200

    Refined the css more

--- index.php
+++ index.php
@@ -1,16 +1,18 @@
 <?php
 require_once __DIR__ . '/data.php';
 require_once __DIR__ . '/header.php';
-
 ?>
 
-<h1>The UEFA Women's Champions League</h1>
 <main>
+    <h1>The UEFA Women's Champions League</h1>
+    <article class="info">
+        <p>Here you can read and learn more about the teams in this year's UEFA Women's Champions League!</p>
+    </article>
     <article class="card-container">
         <?php foreach ($teams as $key => $team): ?>
             <article class="team-card">
                 <h2><?= $key ?> </h2>
-                <img src="<?= $team['logo'] ?>" alt="The teams logo" width="250">
+                <img src="<?= $team['logo'] ?>" alt="The teams logo">
                 <p>League: <?= $team['league'] ?></p>
                 <p>City: <?= $team['city'] ?></p>
                 <p>UEFA coefficient ranking: <?= $team['uefa-coefficient-ranking'] ?></p>
